2d79994b64490475e560485fb582f95c26936695
alejandro <asanchezgiraldo@bauer-media.com.au>
2016-10-11 23:25:12 +1100

    creation script to create DB and Table (#4)
    testing script to do pagination

---
+++ creation_script.php
@@ -0,0 +1,46 @@
+ <?php
+// Connect to MySQL
+    $dbname = 'testreport';
+
+    $link = mysqli_connect("172.17.0.2", "report", "yumyum");
+    if (!$link) {
+        die('Could not connect to MySQL server: ' . mysqli_connect_error());
+    }
+
+    $db_selected = mysqli_select_db($link, $dbname);
+    if (!$db_selected) {
+        $q = 'CREATE DATABASE '.$dbname;
+
+        if (mysqli_query($link, $q)) {
+              echo "Database my_db created successfully\n";
+          } else {
+              echo 'Error creating database: ' . mysqli_error() . "\n";
+          }
+    } else {
+        echo "Database already created\n";
+
+    }
+mysqli_close($link);
+
+// Connect to MYSQL and DB
+    $link = mysqli_connect("172.17.0.2", "report", "yumyum", $dbname, '3306');
+
+    $q = "SELECT ID FROM food";
+    $result = mysqli_query($link, $q);
+
+    if(empty($result)) {
+                    $query = "CREATE TABLE food (
+                              ID char(50),
+                              TEST_NAME text,
+                              DATE text,
+                              STATUS text,
+                              TAGS text,
+                              PRIMARY KEY  (ID)
+                              )";
+                    $result = mysqli_query($link, $query);
+    } else {
+    echo "Somthing has gone wrong" . mysqli_error() . "\n";
+    }
+
+mysqli_close($link);
+?>
---
+++ testing.php
@@ -0,0 +1,58 @@
+<html>
+
+   <head>
+      <title>Paging Using PHP</title>
+   </head>
+   <body>
+      <?php
+        $rec_limit = 10;
+	$link = mysqli_connect("172.17.0.2", "report", "yumyum", "testreport", "3306");
+        $q = 'select * from food order by food.date desc';
+        $res = mysqli_query($link, $q);
+
+	$q = 'select count(id) from food';
+        $counter = mysqli_query($link, $q);
+	$row = mysqli_fetch_array($counter, MYSQLI_NUM);
+	$rec_count = $row[0];
+         if( isset($_GET{'page'} ) ) {
+            $page = $_GET{'page'} + 1;
+            $offset = $rec_limit * $page ;
+         }else {
+            $page = 0;
+            $offset = 0;
+         }
+         $left_rec = $rec_count - ($page * $rec_limit);
+
+        $q = 'select * from food order by food.date desc limit '.$offset.', '.$rec_limit;
+        $res = mysqli_query($link, $q);
+         echo '<table border="1" id="report_table" align="center" width=100%>';
+
+	 while ($row = $res->fetch_assoc()) {
+	        $row_id = $row["ID"];
+                $row_tcname = $row["TEST_NAME"];
+                $row_date = $row["DATE"];
+                $row_tcstatus = $row["STATUS"];
+                $row_tags = $row["TAGS"];
+                // Output a row
+                echo "<tr>";
+                echo "<td>$row_id</td>";
+
+                $urlname = str_replace(' ','_',$row_tcname);
+                echo "<td><a color=white href=refinename.php?name=$urlname>$row_tcname</a></td>";
+
+                echo "<td>$row_date</td>";
+                echo "<td>$row_tcstatus</td>";
+	}
+        if( $left_rec < $rec_limit ) {
+            $last = $page - 2;
+            echo "<a href = \"$_PHP_SELF?page=$last\">Previous 10 Records</a>";
+         }else if( $page > 0 ) {
+            $last = $page - 2;
+            echo "<a href = \"$_PHP_SELF?page=$last\">Previous 10 Records</a> |";
+            echo "<a href = \"$_PHP_SELF?page=$page\">Next 10 Records</a>";
+         }else if( $page == 0 ) {
+            echo "<a href = \"$_PHP_SELF?page=$page\">Next 10 Records</a>";
+         }
+	?>
+   </body>
+</html>
